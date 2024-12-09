63d44fcb0999a6f2b99e101c19445aec7114cc5d
Jordan <jmcgavin13@gmail.com>
2024-12-08 23:58:37 -0500

    feat: add service icons

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,27 +1,18 @@
-import './App.module.css'
+import './styles.css'
+import styles from './App.module.css'
 
 const App = () => {
-  const handleElectronViteClick = () => {
-    window.open('https://electron-vite.github.io', '_blank')
-  }
-
-  const handleReactClick = () => {
-    window.open('https://react.dev', '_blank')
-  }
-
   return (
     <>
-      <h1>
-        Electron Vite
-        <br />
-        +
-        <br />
-        React
-      </h1>
-      <section className="container">
-        <img src="/electron-vite.animate.svg" alt="Vite logo" onClick={handleElectronViteClick} />
-        +
-        <img src="/react.svg" className="react" alt="React logo" onClick={handleReactClick} />
+      <h1>Electron Boilerplate</h1>
+      <h2>Desktop application with Electron, React, Vite & TypeScript</h2>
+      <section className={styles.container}>
+        <img src="/chrome.svg" alt="Chrome logo" />
+        <img src="/electron.svg" alt="Electron logo" />
+        <img src="/nodejs.svg" alt="Node.js logo" />
+        <img src="/react.svg" alt="React logo" />
+        <img src="/typescript.svg" alt="TypeScript logo" />
+        <img src="/vite.svg" alt="Vite logo" />
       </section>
     </>
   )
